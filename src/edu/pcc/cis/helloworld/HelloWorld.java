583215b494edb12aaa9827f7c75727d8a61a8474
Toan Danh <toan.danh@pcc.edu>
2020-02-04 14:54:06 -0800

    print Name

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,6 +4,7 @@ import java.util.Date;
 public class HelloWorld {
     public static void main (String[] args){
         System.out.println("Hello, World");
-        System.out.print(new Date());
+        System.out.println(new Date());
+        System.out.println("Toan, Danh");
     }
 }
